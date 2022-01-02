4fde92596759332b43eaf87c65249296139091ee
Brayan Puentes <brayansmithpuente@gmail.com>
2022-01-02 00:54:04 -0500

    header completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
-import {Link} from "react-router-dom"
-import "./Styles/header.css";
+import { Link } from "react-router-dom";
+import "./styles/header.css";
+import icon from "./sources/icon.png";
 
 const Header = () => {
   const toogle = useRef();
@@ -8,24 +9,45 @@ const Header = () => {
   const toogleHeader = () => {
     toogle.current.classList.toggle("nav-phone-visible");
   };
-
+  const data = "/";
   return (
     <header className="header">
       <nav className="nav">
-        <div className="logo"></div>
+        <div>
+          <Link className="logo" to="/">
+            <img src={icon} alt="" srcset="" />
+          </Link>
+        </div>
 
         <button onClick={toogleHeader} className="toogle-init">
           &#9776;
         </button>
-
         <ul ref={toogle} className="nav-menu">
-          <li className="nav-menu-item"></li>
+          <div className="nav-menu-item">
+            <Link to={data}>Perfil</Link>
+          </div>
+          <li className="nav-menu-item">
+            <Link to="/">Home</Link>
+          </li>
 
-          <li className="nav-menu-item"></li>
+          <li className="nav-menu-item">
+            <Link to="/">Rutas</Link>
+          </li>
 
-          <li className="nav-menu-item"></li>
+          <li className="nav-menu-item">
+            <Link to="/">Cursos</Link>
+          </li>
 
-          <li className="nav-menu-item"></li>
+          <li className="nav-menu-item">
+            <Link to="/">Nosotros</Link>
+          </li>
+
+          <li className="nav-menu-item">
+            <Link to="/">Contacto</Link>
+          </li>
+          <li className="nav-menu-item">
+            <Link to="/">Cerrar sesion</Link>
+          </li>
         </ul>
       </nav>
     </header>
